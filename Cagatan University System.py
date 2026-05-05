4d4c3cf1bdbee66578333c7fb1a835ed693c0679
Cagatan, Rhudd Lawrence D. <cagatanrhudd@gmail.com>
2026-05-05 20:29:16 +0800

    Add files via upload

---
+++ Cagatan University System.py
@@ -0,0 +1,12 @@
+print("Hello Rhuddens! Welcome to Cagatan University System!\n")
+
+print("Please Login:")
+id_number = input("ID Number: ")
+password = input("Password: ")
+
+if id_number == "2025010203" and password == "gwaposirhuddomg":
+    print("\nLogin Successful! Welcome Rhuddens!")
+    print("Coming Soon pa ang features mwehehehe")
+
+else:
+    print("Gagi ka! Wrong ID Number or Password! Try Again!")
