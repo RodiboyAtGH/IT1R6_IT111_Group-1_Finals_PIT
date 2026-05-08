fae56dba8171f260ba08b3dc086b945c34211ece
Lloren, Justine Ally A. <lloren.justineally@gmail.com>
2026-05-08 13:26:57 +0800

    Implement student record addition with ID hashing, Addition of student record, compiling records in a file
    Refactor student record system with hashing and file handling.

--- Cagatan University System.py
+++ Cagatan University System.py
@@ -1,5 +1,54 @@
-print("Hello Rhuddens! Welcome to Cagatan University System!\n")
+import hashlib
+import os
 
+print("======== STUDENT RECORD SYSTEM ========")
+print("\nHello Rhuddens! Welcome to Cagatan University Student Record System!\n")
+
+file_name = "students.txt"
+
+def hash_id(student_ID):
+    return hashlib.shan256(student_ID.encode()).hexdigest()
+
+def add student():
+    print("\n===== ADD STUDENT =====\n")
+    print("Note: Please fill out the following information: /n")
+
+name = input("Enter Name: ")
+    age = input("Enter Age: ")
+    student_ID = input("Enter Student ID: ")
+    password = input("Enter Password: ")
+    section = input("Enter Section: ")
+    year_level = input("Enter Year Level: ")
+    course = input("Enter Course: ")
+
+hash_id = hash_id(student_ID)
+
+if os.path.exist(file_name):
+    with open(file_name, "r") as file:
+        records = file.readlines()
+
+    for record in records;
+        data = record.strip().split("|")
+
+if data[0] == hashed_id:
+    print("Student ID already exists!")
+    return
+
+with open(file_name, "a") as file:
+    file.write(
+        f"{hashed_id}|{name}|{age}|{student_ID}|"
+        f"{password}|{section}|{year_level}|{course}\n"
+    )
+
+print("STUDENT RECORD ADDED SUCESSFULLY!")
+
+
+      
+
+    
+
+
+# NOT YET EDITED
 print("Please Login:")
 id_number = input("ID Number: ")
 password = input("Password: ")
