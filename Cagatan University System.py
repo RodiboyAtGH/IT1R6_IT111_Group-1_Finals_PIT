dc550c512708237db2b6669fb3146db26c826a52
Kiera Jhasmine Waban <kierajhasmine101@gmail.com>
2026-05-10 00:32:37 +0800

    Add view student records feature

--- Cagatan University System.py
+++ Cagatan University System.py
@@ -9,31 +9,19 @@ def main_menu():
     print("======================================")
     print("Please select an option:")
     print("1. Add Student")
-    print("2. Exit")
+    print("2. View Student Records")
+    print("3. Exit")
     print("======================================")  
-
-def main():
-    while True:
-        main_menu()
-        choice = input("Enter your choice: \n")
-        
-        if choice == "1":
-            add_student()
-        elif choice == "2":
-            print("Exiting program. Goodbye Rhuddens!")
-            break
-        else:
-            print("Invalid. Please try again.")
 
 #add student option
 file_name = "students.txt"
 
 def hash_id(student_ID):
-    return hashlib.shan256(student_ID.encode()).hexdigest()
+    return hashlib.sha256(student_ID.encode()).hexdigest()
 
 def add_student():
     print("\n===== ADD STUDENT =====\n")
-    print("Note: Please fill out the following information: /n")
+    print("Note: Please fill out the following information: \n")
     
     name = input("Enter Name: ")
     age = input("Enter Age: ")
@@ -43,31 +31,67 @@ def add_student():
     year_level = input("Enter Year Level: ")
     course = input("Enter Course: ")
     
-    hash_id = hash_id(student_ID)
+    hashed_id = hash_id(student_ID)
     
-    if os.path.exist(file_name):
+    #check duplicates
+    if os.path.exists(file_name):
         with open(file_name, "r") as file:
             records = file.readlines()
-            
+
             for record in records:
                 data = record.strip().split("|")
         
                 if data[0] == hashed_id:
                     print("Student ID already exists!")
                     return
-
-        with open(file_name, "a") as file:
-            file.write(
-                f"{hashed_id}|{name}|{age}|{student_ID}|"
-                f"{password}|{section}|{year_level}|{course}\n"
+                
+    with open(file_name, "a") as file:
+        file.write(
+            f"{hashed_id}|{name}|{age}|{student_ID}|"
+            f"{password}|{section}|{year_level}|{course}\n"
                 )
+                            
+    print("STUDENT RECORD ADDED SUCESSFULLY!")
+
+#view student records
+def view_students():
+
+    print("\n======== STUDENT RECORDS ========\n")
+
+    if not os.path.exists(file_name):
+        print("No student records found!!\n")
+        return
+    
+    with open(file_name, "r") as file:
+        records = file.readlines()
+
+
+        for record in records:
+            data = record.strip().split("|")
+
+            print(
+                f"Name: {data[1]} | "
+                f"Age: {data[2]} | "
+                f"Course: {data[7]} | "
+                f"Section: {data[5]} | "
+                f"ID: {data[3]}"
+            )
         
-            print("STUDENT RECORD ADDED SUCESSFULLY!")
-
-#calling area
-
+def main():
+    while True:
+        main_menu()
+        choice = input("Enter your choice: \n")
+        
+        if choice == "1":
+            add_student()
+        elif choice == "2":
+            view_students()
+        elif choice == "3":
+            print("Exiting program. Goodbye Rhuddens!")
+            break
+        else:
+            print("Invalid. Please try again.")
 main()
-            
 
       
 
