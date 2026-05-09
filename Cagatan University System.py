7e817ac70d4018fefb30105a18202956adb85dbb
Kiera Jhasmine Waban <kierajhasmine101@gmail.com>
2026-05-09 17:06:05 +0800

    Correct student addition function and error handling
    Fixed syntax errors and improved student addition logic.

--- Cagatan University System.py
+++ Cagatan University System.py
@@ -9,39 +9,41 @@ file_name = "students.txt"
 def hash_id(student_ID):
     return hashlib.shan256(student_ID.encode()).hexdigest()
 
-def add student():
+def add_student():
     print("\n===== ADD STUDENT =====\n")
     print("Note: Please fill out the following information: /n")
-
-name = input("Enter Name: ")
+    
+    name = input("Enter Name: ")
     age = input("Enter Age: ")
     student_ID = input("Enter Student ID: ")
     password = input("Enter Password: ")
     section = input("Enter Section: ")
     year_level = input("Enter Year Level: ")
     course = input("Enter Course: ")
+    
+    hash_id = hash_id(student_ID)
+    
+    if os.path.exist(file_name):
+        with open(file_name, "r") as file:
+            records = file.readlines()
+            
+            for record in records:
+                data = record.strip().split("|")
+        
+                if data[0] == hashed_id:
+                    print("Student ID already exists!")
+                    return
 
-hash_id = hash_id(student_ID)
+        with open(file_name, "a") as file:
+            file.write(
+                f"{hashed_id}|{name}|{age}|{student_ID}|"
+                f"{password}|{section}|{year_level}|{course}\n"
+                
+                )
+        
+            print("STUDENT RECORD ADDED SUCESSFULLY!")
 
-if os.path.exist(file_name):
-    with open(file_name, "r") as file:
-        records = file.readlines()
-
-    for record in records;
-        data = record.strip().split("|")
-
-if data[0] == hashed_id:
-    print("Student ID already exists!")
-    return
-
-with open(file_name, "a") as file:
-    file.write(
-        f"{hashed_id}|{name}|{age}|{student_ID}|"
-        f"{password}|{section}|{year_level}|{course}\n"
-    )
-
-print("STUDENT RECORD ADDED SUCESSFULLY!")
-
+            
 
       
 
