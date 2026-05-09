4ab3d13c1afaa48b2e3e2db348d84876f11438df
Kiera Jhasmine Waban <kierajhasmine101@gmail.com>
2026-05-09 21:12:47 +0800

    Implement main menu and student addition features
    Added main menu functionality and improved student record addition process.

--- Cagatan University System.py
+++ Cagatan University System.py
@@ -42,7 +42,30 @@ def add_student():
                 )
         
             print("STUDENT RECORD ADDED SUCESSFULLY!")
+            print("\nHello Rhuddens!\n\nWelcome to Cagatan University Student Record System!\n")
 
+//menu option
+def main_menu():
+    print("======================================")
+    print("Please select an option:")
+    print("1. Add Student")
+    print("2. Exit")
+    print("======================================")  
+
+def main():
+    while True:
+        main_menu()
+        choice = input("Enter your choice: \n")
+        
+        if choice == "1":
+            add_student()
+        elif choice == "2":
+            print("Exiting program. Goodbye Rhuddens!")
+            break
+        else:
+            print("Invalid. Please try again.")
+
+//add student option
             
 
       
